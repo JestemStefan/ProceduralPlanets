06530a6e12eeb15146e2cd3e85dac6f412a43e4b
JestemStefan <pudipudi2x@gmail.com>
2021-08-26 22:39:53 +0200

    Small optimizations

--- Planet.gd
+++ Planet.gd
@@ -9,6 +9,8 @@ export var _persistance: float = 0.8 setget set_persistance
 export var _lacunarity: float = 2 setget set_lacunarity
 export var _noise_strength: float = 0.5 setget set_noise_strength
 
+var arr_mesh_save: ArrayMesh = ArrayMesh.new()
+
 var mdt: MeshDataTool = MeshDataTool.new()
 var st: SurfaceTool = SurfaceTool.new()
 var ground_noise: OpenSimplexNoise = OpenSimplexNoise.new()
@@ -18,10 +20,23 @@ export var surface_colors: Gradient
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
+	
 
 
 func _process(delta: float) -> void:
-	pass
+	rotate_y(delta * 0.01)
+
+
+func make_new_mesh():
+	var new_mesh: CubeMesh = CubeMesh.new()
+	
+	new_mesh.subdivide_depth = resolution
+	new_mesh.subdivide_height = resolution
+	new_mesh.subdivide_width = resolution
+	
+	arr_mesh_save.surface_remove(0)
+	arr_mesh_save.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, new_mesh.get_mesh_arrays())
+	
 
 
 func generate_sphere_mesh():
@@ -30,16 +45,9 @@ func generate_sphere_mesh():
 	ground_noise.period = _period
 	ground_noise.persistence = _persistance
 	ground_noise.lacunarity = _lacunarity
-
-	var original_mesh: CubeMesh = CubeMesh.new()
-	
-	original_mesh.subdivide_depth = resolution
-	original_mesh.subdivide_height = resolution
-	original_mesh.subdivide_width = resolution
 	
 	var array_mesh: ArrayMesh = ArrayMesh.new()
-	
-	array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, original_mesh.get_mesh_arrays())
+	array_mesh = arr_mesh_save.duplicate()
 	
 	mdt.create_from_surface(array_mesh, 0)
 	
@@ -75,28 +83,7 @@ func generate_sphere_mesh():
 	if end_idx > vertex_count:
 		end_idx = vertex_count
 	
-	for i in range(start_idx, end_idx):
-		
-		var vertex: Vector3 = mdt.get_vertex(i)
-		var v_normal: Vector3 = mdt.get_vertex_normal(i)
-		vertex = vertex.normalized()
-		mdt.set_vertex_normal(i, vertex)
-		
-		var elevation: float = ground_noise.get_noise_3dv(vertex)
-		
-		#min_elevation = min(min_elevation, elevation)
-		#max_elevation = max(max_elevation, elevation)
-		
-		var point_on_gradient: float = range_lerp(elevation, -0.5, 0.5, 0, 1)
-		
-		mdt.set_vertex_color(i, surface_colors.interpolate(point_on_gradient))
-		
-		if elevation < 0:
-			elevation = 0
-		
-		vertex += mdt.get_vertex_normal(i) * elevation * _noise_strength
-		
-		mdt.set_vertex(i, vertex)
+	threaded_update_mesh([start_idx, end_idx])
 	
 	for t in thread_activated:
 		(t as Thread).wait_to_finish()
@@ -108,36 +95,36 @@ func generate_sphere_mesh():
 	st.begin(Mesh.PRIMITIVE_TRIANGLES)
 	st.add_smooth_group(true)
 	st.append_from(array_mesh, 0, transform)
-	
-	
 	st.generate_normals()
+
 	array_mesh.surface_remove(0)
 	st.commit(array_mesh)
 	
 	$PlanetMesh.mesh = array_mesh
-
+	
+	
 
 func threaded_update_mesh(start_stop: Array):
 	for i in range(start_stop[0], start_stop[1]):
 		
 		var vertex: Vector3 = mdt.get_vertex(i)
-		var v_normal: Vector3 = mdt.get_vertex_normal(i)
 		vertex = vertex.normalized()
 		mdt.set_vertex_normal(i, vertex)
 		
 		var elevation: float = ground_noise.get_noise_3dv(vertex)
+		elevation = sign(elevation) * pow(elevation, 2)
 		
-		#min_elevation = min(min_elevation, elevation)
-		#max_elevation = max(max_elevation, elevation)
-		
-		var point_on_gradient: float = range_lerp(elevation, -0.5, 0.5, 0, 1)
-		
+		var point_on_gradient: float = range_lerp(elevation, -0.25, 0.25, 0, 1)
 		mdt.set_vertex_color(i, surface_colors.interpolate(point_on_gradient))
 		
-		if elevation < 0:
-			elevation = 0
-		
-		vertex += mdt.get_vertex_normal(i) * elevation * _noise_strength
+		if elevation >= 0:
+			var v_normal: Vector3 = mdt.get_vertex_normal(i)
+			
+			
+			#min_elevation = min(min_elevation, elevation)
+			#max_elevation = max(max_elevation, elevation)
+			
+			vertex += mdt.get_vertex_normal(i) * elevation * _noise_strength
 		
 		mdt.set_vertex(i, vertex)
 	
@@ -148,29 +135,37 @@ func get_resolution():
 	return resolution
 	
 func set_resolution(res):
-	resolution = res
-	generate_sphere_mesh()
+	if res != resolution:
+		resolution = res
+		make_new_mesh()
+		generate_sphere_mesh()
 
 func set_seed(s):
-	_seed = s
-	generate_sphere_mesh()
+	if s != _seed:
+		_seed = s
+		generate_sphere_mesh()
 	
 func set_octaves(o):
-	_octaves = o
-	generate_sphere_mesh()
+	if o != _octaves:
+		_octaves = o
+		generate_sphere_mesh()
 
 func set_period(period):
-	_period = period
-	generate_sphere_mesh()
+	if period != _period:
+		_period = period
+		generate_sphere_mesh()
 
 func set_persistance(p):
-	_persistance = p
-	generate_sphere_mesh()
+	if p != _persistance:
+		_persistance = p
+		generate_sphere_mesh()
 	
 func set_lacunarity(l):
-	_lacunarity = l
-	generate_sphere_mesh()
+	if l != _lacunarity:
+		_lacunarity = l
+		generate_sphere_mesh()
 	
 func set_noise_strength(ns):
-	_noise_strength = ns
-	generate_sphere_mesh()
+	if ns != _noise_strength:
+		_noise_strength = ns
+		generate_sphere_mesh()
